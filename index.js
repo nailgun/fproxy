e38d22a660428d6771e3cc64ee2163296f50b6e1
Dmitry Bashkatov <dbashkatov@gmail.com>
2015-11-20 14:24:18 +0300

    clean

--- index.js
+++ index.js
@@ -3,17 +3,16 @@ var net = require('net');
 var util = require('util');
 
 // TODO: agent?
-// TODO: see TODOs from index.js
 // TODO: logging, options
 
 // Create an HTTP tunneling proxy
 var proxy = http.createServer(function (req, res) {
-    console.log(req.url);
+    console.log(req.url);  // TODO: logging
     protect(handleRequest, res)(req, res);
 });
 
 proxy.on('connect', function(req, clientSocket, head) {
-    console.log(req.url);
+    console.log(req.url);  // TODO: logging
     var res = new http.ServerResponse(req);
     res.assignSocket(clientSocket);
     protect(handleRequest, res)(req, res, head);
@@ -72,58 +71,52 @@ function handleRequest (req, res, head) {
 }
 
 function getDownstreamProxy (req, res) {
-    var host, port, parts;
-
-    var header = req.headers['proxy-downstream-proxy'];
-    if (header) {
-        delete req.headers['proxy-downstream-proxy'];
-
-        parts = header.split(':', 2);
-        if (parts.length != 2) {
-            throw new VisibleError('Invalid Proxy-Downstream-Proxy header', null, 400);
-        }
-
-        host = parts[0];
-        port = parts[1];
+    /*
+     * Downstream proxy configuration is taken from Proxy-Authorization header.
+     * We can't use custom header here, because most HTTPS clients don't send any
+     * HTTP header to a proxy except Proxy-Authorization.
+     *
+     * For example, to connect via fproxy running at 127.0.0.2:8833
+     * and via downstream proxy user:pass@127.0.0.1:8080, issue the following command:
+     *
+     * curl -vx http://127.0.0.1,8080,user:pass@127.0.0.2:8833/ http://httpbin.org/headers
+     */
+    var header = req.headers['proxy-authorization'];
+    if (!header) {
+        throw new VisibleError(res, 'Missing Proxy-Downstream-Proxy or Proxy-Authorization header', null, 400);
     }
-    else {
-        header = req.headers['proxy-authorization'];
-        if (!header) {
-            throw new VisibleError(res, 'Missing Proxy-Downstream-Proxy or Proxy-Authorization header', null, 400);
-        }
-        delete req.headers['proxy-authorization'];
+    delete req.headers['proxy-authorization'];
 
-        parts = header.split(' ', 2);
-        if (parts.length != 2) {
-            throw new VisibleError('Invalid Proxy-Authorization header', null, 400);
+    var parts = header.split(' ', 2);
+    if (parts.length != 2) {
+        throw new VisibleError('Invalid Proxy-Authorization header', null, 400);
 
-        }
-        var authMethod = parts[0],
-            downstream = parts[1];
+    }
+    var authMethod = parts[0],
+        downstream = parts[1];
 
-        if (authMethod.toLowerCase() != 'basic') {
-            throw new VisibleError(res, 'Invalid Proxy-Authorization method (only basic supported)', null, 400);
-        }
+    if (authMethod.toLowerCase() != 'basic') {
+        throw new VisibleError(res, 'Invalid Proxy-Authorization method (only basic supported)', null, 400);
+    }
 
-        downstream = new Buffer(downstream, 'base64').toString();
-        parts = downstream.split(',', 3);
-        if (parts.length != 3) {
-            throw new VisibleError('Invalid Proxy-Authorization header format', null, 400);
-        }
+    downstream = new Buffer(downstream, 'base64').toString();
+    parts = downstream.split(',', 3);
+    if (parts.length != 3) {
+        throw new VisibleError('Invalid Proxy-Authorization header format', null, 400);
+    }
 
-        host = parts[0];
-        port = parts[1];
+    var host = parts[0],
+        port = parts[1],
+        auth = parts[2];
 
-        var auth = parts[2];
-        if (auth) {
-            req.headers['proxy-authorization'] = 'Basic ' + new Buffer(auth).toString('base64');
-        }
+    if (auth) {
+        req.headers['proxy-authorization'] = 'Basic ' + new Buffer(auth).toString('base64');
     }
 
     return {
         host: host,
         port: port
-    }
+    };
 }
 
 function protect (func, res) {
@@ -156,7 +149,7 @@ function fail (res, message, code) {
         'Content-Type': 'text/plain'
     });
 
-    res.end('Forward-Proxy: ' + (message || 'Error'));
+    res.end('fproxy: ' + (message || 'Error'));
 }
 
 function VisibleError (message, reason, code) {
